36fa223235ab69e4798829fcb5b113ea2619dcab
Friday Davis <55281305+sirdavis99@users.noreply.github.com>
2022-05-19 13:09:31 +0100

    Update index.py

---
+++ teaching-1/index.py
@@ -0,0 +1,7 @@
+# for loop to count to 10
+
+listOfFruits = ['apple', 'banana', 'mango', 'pineapple', 'orange']
+count = 0
+while count < len(listOfFruits):
+    print(listOfFruits[count])
+    count+=1
