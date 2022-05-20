3eaeca106e232e0782dece9e65280070fefc2e2c
Friday Davis <55281305+sirdavis99@users.noreply.github.com>
2022-05-20 14:43:18 +0100

    Create how_to_function.py

---
+++ teaching-1/how_to_function.py
@@ -0,0 +1,5 @@
+# create two functions 
+
+# 1 check if number is even or odd number so returns 'number is even|odd'
+
+# 2 print the x10 of any number assigned to the function eg anyName(50) returns 500
