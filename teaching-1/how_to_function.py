74b147cf4ac85f763bcf9585761ee0618ab257a4
olayimika10 <105859762+olayimika10@users.noreply.github.com>
2022-05-31 17:55:17 +0100

    last. assessment

--- teaching-1/how_to_function.py
+++ teaching-1/how_to_function.py
@@ -3,3 +3,26 @@
 # 1 check if number is even or odd number so returns 'number is even|odd'
 
 # 2 print the x10 of any number assigned to the function eg anyName(50) returns 500
+
+
+def number_type(number):
+ if (number % 2 == 0): 
+     print('number is even')
+     
+ else:
+     print('the number is odd')
+
+# number_type(31)
+def any_number(number):
+    x = number * 10
+    print(x)
+
+any_number(50)
+
+     
+    
+        
+        
+        
+        
+    
--- teaching-1/how_to_import.py
+++ teaching-1/how_to_import.py
@@ -1,5 +1,9 @@
 import how_to_while as hw;
+import how_to_function as hf;
+import how_to_function as hg
 
 print(hw.name)
 
 hw.listFruits();
+hf.number_type(56)
+hg.any_number
